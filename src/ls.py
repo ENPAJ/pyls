0ce0372ccdee0136c782fde021bc9929e575519f
Pauline Nkouane <paulinenkouane15@gmail.com>
2023-11-30 17:07:58 +0100

    :lock: security and OS modif :sparkles:

--- src/ls.py
+++ src/ls.py
@@ -1,5 +1,15 @@
 import os
 
-repertoire = input("Entrez le chemin du repertoire :")
+repertoire = input("Entrez le chemin du répertoire : ")
 
-os.system(f"ls {repertoire}")
+# Valider ou échapper l'entrée utilisateur pour éviter les injections de commandes
+# Utilisez toujours des méthodes plus sécurisées selon le contexte d'utilisation réel.
+repertoire = os.path.abspath(os.path.expanduser(repertoire))
+
+# Vérifiez le système d'exploitation pour choisir la commande appropriée
+if os.name == 'posix':
+    os.system(f"ls {repertoire}")
+elif os.name == 'nt':
+    os.system(f"dir {repertoire}")
+else:
+    print("Système d'exploitation non pris en charge.")
